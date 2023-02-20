9e42d82fedbc4e0becd2711d6ab6c0101cdbbbec
Francesco Giusti <fra.giusti.job@gmail.com>
2023-02-20 12:28:48 +0100

    [WIP] added TODOS

--- src/main/java/fra/giusti/job/component/controller/ProductController.java
+++ src/main/java/fra/giusti/job/component/controller/ProductController.java
@@ -4,8 +4,10 @@ import fra.giusti.job.api.ProductApi;
 import fra.giusti.job.component.command.CreateProductCommand;
 import fra.giusti.job.component.mapper.ProductMapper;
 import fra.giusti.job.model.Product;
+import fra.giusti.job.model.ProductList;
 import fra.giusti.job.model.ProductRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,8 +18,17 @@ public class ProductController implements ProductApi {
 
     @Override
     public ResponseEntity<Product> createProduct(ProductRequest request) {
-        var response = createProductCommand.execute(ProductMapper.map(request));
+        return ResponseEntity.ok(ProductMapper.map(createProductCommand.execute(ProductMapper.map(request))));
+    }
+    @Override
+    public ResponseEntity<ProductList> getProductByFilter(Long id) {
+        //TODO: implement this Api
+        return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+    }
 
-        return ResponseEntity.ok(ProductMapper.map(response));
+    @Override
+    public ResponseEntity<Product> updateProduct(ProductRequest productRequest) {
+        //TODO: implement this Api
+        return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
     }
 }
